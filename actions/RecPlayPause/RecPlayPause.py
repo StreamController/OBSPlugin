f700d7793a98d35758bd43378878f5c69f6fbfb8
Core447 <jmulmann@protonmail.com>
2024-05-10 17:33:23 +0200

    Update constructors

--- actions/RecPlayPause/RecPlayPause.py
+++ actions/RecPlayPause/RecPlayPause.py
@@ -6,10 +6,8 @@ from src.backend.PluginManager.PluginBase import PluginBase
 import os
 
 class RecPlayPause(OBSActionBase):
-    def __init__(self, action_id: str, action_name: str,
-                 deck_controller: "DeckController", page: Page, coords: str, plugin_base: PluginBase):
-        super().__init__(action_id=action_id, action_name=action_name,
-            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.current_state = -1
 
     def on_ready(self):
